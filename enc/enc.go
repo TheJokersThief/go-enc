ad7186c9785b1d3ffd8ee43429d3f9ca86fdf355
Evan Smith <esincork@gmail.com>
2018-02-17 11:29:02 +0000

    Add support plan for config types

--- enc/enc.go
+++ enc/enc.go
@@ -1,20 +1,63 @@
 package main
 
-import ()
+import (
+	"errors"
+)
 
 type ENCNodegroup struct {
-	Parent     string                  `json:"parent"`
-	Classes    *map[string]interface{} `json:"classes"`
-	Nodes      *[]string               `json:"nodes"`
-	Parameters *map[string]interface{} `json:"parameter"`
+	Parent     string                 `json:"parent", yaml:"parent"`
+	Classes    map[string]interface{} `json:"classes", yaml:"classes"`
+	Nodes      []string               `json:"nodes", yaml:"nodes"`
+	Parameters map[string]interface{} `json:"parameter", yaml:"parameter"`
 }
 
 type ENC struct {
-	ENCNodes *map[string]ENCNodegroup `json:"nodes"`
+	Nodegroups map[string]ENCNodegroup `json:"nodes", yaml:"nodes"`
+	ConfigType string
 }
 
-func NewENC() *ENC {
+func NewENC(config_type string) *ENC {
 	return &ENC{
-		ENCNodes: &map[string]ENCNodegroup{},
+		Nodegroups: map[string]ENCNodegroup{},
+		ConfigType: config_type,
 	}
 }
+
+func (enc *ENC) AddNodegroup(name string, parent string, classes map[string]interface{}, nodes []string, params map[string]interface{}) (*ENCNodegroup, error) {
+	nodegroup := ENCNodegroup{
+		Parent:     parent,
+		Classes:    classes,
+		Nodes:      nodes,
+		Parameters: params,
+	}
+
+	if _, ok := enc.Nodegroups[name]; !ok {
+		enc.Nodegroups[name] = nodegroup
+	} else {
+		return &ENCNodegroup{}, errors.New("Nodegroup already exists")
+	}
+
+	return &nodegroup, nil
+}
+
+func (enc *ENC) RemoveNodegroup(name string) (*ENCNodegroup, error) {
+	if _, ok := enc.Nodegroups[name]; !ok {
+		return &ENCNodegroup{}, errors.New("Nodegroup does not exist")
+	} else {
+		nodegroup := enc.Nodegroups[name]
+		delete(enc.Nodegroups, name)
+		return &nodegroup, nil
+	}
+}
+
+func (enc *ENC) GetNodegroup()    {}
+func (enc *ENC) AddNode()         {}
+func (enc *ENC) RemoveNode()      {}
+func (enc *ENC) GetNode()         {}
+func (enc *ENC) AddParameter()    {}
+func (enc *ENC) SetParameter()    {}
+func (enc *ENC) RemoveParameter() {}
+func (enc *ENC) AddClass()        {}
+func (enc *ENC) RemoveClass()     {}
+func (enc *ENC) SetClasses()      {}
+func (enc *ENC) SetParent()       {}
